88ab5cc99ff7d95f1837808df40b9df6644a0bd8
Douglas <7.douglas@gmail.com>
2022-04-27 23:45:22 -0300

    calendar adjustments

--- filters.js
+++ filters.js
@@ -57,3 +57,14 @@ function show(e){
         // titulo.textOverflow = "ellipis"
     }
 }
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("buttons");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
